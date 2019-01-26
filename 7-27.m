b439bcca704d5267fbbef3195c9157e7a30671d0
Jay Grinols <jaygrinols@gmail.com>
2019-01-26 12:25:42 -0800

    im not sure what this is lol

--- 7-27.m
+++ 7-27.m
@@ -1,5 +1,10 @@
 f = @(x, y) -8*x + x.^2 + 12*y + 4*y.^2 - 2*x.*y;
 %a) graphical
+[X, Y] = meshgrid(linspace(-5, 5, 20), linspace(-5, 5, 20));
+Z = f(X, Y);
+surf(X, Y, Z)
+colorbar
+pause
 
 %b) fminsearch
 
